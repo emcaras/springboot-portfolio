c53af2fcf22eaa0583b107e9506c8d8492b33a05
Emiliano HP <emilianocarranzar7@gmail.com>
2026-04-30 17:18:35 -0600

    first commit

---
+++ src/main/java/com/emcaras/portfolio/model/Education.java
@@ -0,0 +1,19 @@
+package com.emcaras.portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Education {
+    private Long id;
+    private String degree;
+    private String institution;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String description;
+    private Long personalInfoId;
+}
---
+++ src/main/java/com/emcaras/portfolio/model/Experience.java
@@ -0,0 +1,19 @@
+package com.emcaras.portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Experience {
+    private Long id;
+    private String jobTitle;
+    private String companyName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String description;
+    private Long personalInfoId;
+}
